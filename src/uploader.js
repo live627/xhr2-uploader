55d1e9938dfeea4ccd03484bcfec83cf1198ef9a
John Rayes <live627@gmail.com>
2015-05-14 16:00:33 -0700

    Add a simple string hash

--- src/uploader.js
+++ src/uploader.js
@@ -1,3 +1,13 @@
+String.prototype.hashCode = function () {
+	var h = 0, i = 0, l = this.length;
+	if (l === 0) return h;
+	for (; i < l; i++) {
+		h = ((h << 5) - h) + this.charCodeAt(i);
+		h |= 0; // Convert to 32bit integer
+	}
+	return h;
+};
+
 (function ($) {
 
 	$.fn.uploader = function (o)
@@ -12,7 +22,7 @@
 			$.each(files, function (index, file)
 			{
 				var
-					name = file.name, size = file.size, type = file.type, hash = hex_sha1(name + Math.random()),
+					name = file.name, size = file.size, type = file.type, hash = name.hashCode() + Math.random(),
 
 					xhrUploadEvents = {
 
